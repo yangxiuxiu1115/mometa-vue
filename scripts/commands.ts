6ee8ef86b7116c13258dd262652c7eb17cef1c01
‘xiuxiu-yang’ <1974364190@qq.com>
2023-01-22 16:33:59 +0800

    chore: typecheck构建

---
+++ scripts/commands.ts
@@ -0,0 +1,3 @@
+const commands = ['pnpm -F @mometa-vue/editor run typecheck', 'pnpm -F @mometa-vue/example run typecheck']
+
+export default commands
---
+++ scripts/typecheck.ts
@@ -0,0 +1,6 @@
+import { execSync } from 'child_process'
+import commands from './commands'
+
+for (const command of commands) {
+  execSync(command)
+}
