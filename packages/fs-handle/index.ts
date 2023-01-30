3ea9857bd674988fad899ea3ecb15fa33e91b892
‘xiuxiu-yang’ <1974364190@qq.com>
2023-01-30 18:02:09 +0800

    feat: 完成nodeOptMiddleware

--- packages/fs-handle/index.ts
+++ packages/fs-handle/index.ts
@@ -1,1 +1,19 @@
-export * from './src/index'
+import type { Connect } from 'vite'
+import { middlewares } from './src/index'
+
+const nodeOptMiddleware: Connect.NextHandleFunction = (req, res, next) => {
+  if (req.url === '/nodeopt') {
+    let i = -1
+    const dispatch: Connect.NextFunction = async (index: number) => {
+      if (i >= index) console.warn('请勿重复调用next')
+      i = index
+      const fn = middlewares[index]
+      fn && fn(req, res, () => dispatch(++index))
+    }
+    dispatch(0)
+  } else {
+    next()
+  }
+}
+
+export default nodeOptMiddleware
