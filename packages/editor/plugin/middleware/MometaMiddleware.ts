ca8456b9efa5feed3477d9f14b0a46bc17cca4ad
‘xiuxiu-yang’ <1974364190@qq.com>
2023-02-10 20:48:29 +0800

    fix: 修复了Mometa-plugin发布后的引用路劲问题

--- packages/editor/plugin/middleware/MometaMiddleware.ts
+++ packages/editor/plugin/middleware/MometaMiddleware.ts
@@ -1,11 +1,18 @@
 import { readFileSync } from 'node:fs'
-import { MOMETAURL, resolve } from './const'
+import { normalizePath } from 'vite'
 import type { Connect } from 'vite'
+import { MOMETAURL, resolve } from './const'
+
+const DEFINE_OPTIONS = '/${MOMETA_BASE_URL}'
 
 const MometaMiddleware: Connect.NextHandleFunction = (req, res, next) => {
   if (req.url === `${MOMETAURL}`) {
-    const mometa = readFileSync(resolve(`../..${MOMETAURL}/editor/index.html`))
-    res.end(mometa)
+    const mometa = readFileSync(resolve(`../..${MOMETAURL}/editor/index.html`), { encoding: 'utf-8' })
+    const realMometa = mometa.replaceAll(
+      encodeURI(DEFINE_OPTIONS),
+      normalizePath(`/@fs/${resolve('../../__mometa/editor')}`)
+    )
+    res.end(realMometa)
     return
   }
   next()
