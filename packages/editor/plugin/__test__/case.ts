ec8a3c33f5a3f8fe750e780a3a892bc50e5b70af
RicardoErii <1974364190@qq.com>
2023-02-16 18:46:21 +0800

    test: 修改mometa测试用例

--- packages/editor/plugin/__test__/case.ts
+++ packages/editor/plugin/__test__/case.ts
@@ -31,13 +31,13 @@ export const commonTest = `<script setup lang="ts">
 </script>
 
 <template>
-  <div :mometa="{ start: { column: 3, line: 5, offset: 49}, end: { column: 9, line: 13, offset: 348}, isSelfClosing: false, filename: 'app', isComponent: false}">
-    <a :mometa="{ start: { column: 5, line: 6, offset: 59}, end: { column: 9, line: 9, offset: 211}, isSelfClosing: false, filename: 'app', isComponent: false}" href="https://vitejs.dev" target="_blank">
-      <img :mometa="{ start: { column: 7, line: 7, offset: 111}, end: { column: 59, line: 7, offset: 163}, isSelfClosing: true, filename: 'app', isComponent: false}" src="/vite.svg" class="logo" alt="Vite logo" />
-      <span :mometa="{ start: { column: 7, line: 8, offset: 170}, end: { column: 23, line: 8, offset: 186}, isSelfClosing: false, filename: 'app', isComponent: false}">asd</span><span :mometa="{ start: { column: 23, line: 8, offset: 186}, end: { column: 39, line: 8, offset: 202}, isSelfClosing: false, filename: 'app', isComponent: false}">asd</span>
+  <div :mometa="{ start: { column: 3, line: 5, offset: 49}, end: { column: 9, line: 13, offset: 348}, isSelfClosing: false, filename: 'app', isComponent: false, name: 'div'}">
+    <a :mometa="{ start: { column: 5, line: 6, offset: 59}, end: { column: 9, line: 9, offset: 211}, isSelfClosing: false, filename: 'app', isComponent: false, name: 'a'}" href="https://vitejs.dev" target="_blank">
+      <img :mometa="{ start: { column: 7, line: 7, offset: 111}, end: { column: 59, line: 7, offset: 163}, isSelfClosing: true, filename: 'app', isComponent: false, name: 'img'}" src="/vite.svg" class="logo" alt="Vite logo" />
+      <span :mometa="{ start: { column: 7, line: 8, offset: 170}, end: { column: 23, line: 8, offset: 186}, isSelfClosing: false, filename: 'app', isComponent: false, name: 'span'}">asd</span><span :mometa="{ start: { column: 23, line: 8, offset: 186}, end: { column: 39, line: 8, offset: 202}, isSelfClosing: false, filename: 'app', isComponent: false, name: 'span'}">asd</span>
     </a>
-    <a :mometa="{ start: { column: 5, line: 10, offset: 216}, end: { column: 9, line: 12, offset: 339}, isSelfClosing: false, filename: 'app', isComponent: false}" href="https://vuejs.org/" target="_blank">
-      <img :mometa="{ start: { column: 7, line: 11, offset: 268}, end: { column: 69, line: 11, offset: 330}, isSelfClosing: true, filename: 'app', isComponent: false}" src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    <a :mometa="{ start: { column: 5, line: 10, offset: 216}, end: { column: 9, line: 12, offset: 339}, isSelfClosing: false, filename: 'app', isComponent: false, name: 'a'}" href="https://vuejs.org/" target="_blank">
+      <img :mometa="{ start: { column: 7, line: 11, offset: 268}, end: { column: 69, line: 11, offset: 330}, isSelfClosing: true, filename: 'app', isComponent: false, name: 'img'}" src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
 </template>
@@ -91,13 +91,13 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div :mometa="{ start: { column: 3, line: 6, offset: 102}, end: { column: 9, line: 14, offset: 401}, isSelfClosing: false, filename: 'app', isComponent: false}">
-    <a :mometa="{ start: { column: 5, line: 7, offset: 112}, end: { column: 9, line: 10, offset: 264}, isSelfClosing: false, filename: 'app', isComponent: false}" href="https://vitejs.dev" target="_blank">
-      <img :mometa="{ start: { column: 7, line: 8, offset: 164}, end: { column: 59, line: 8, offset: 216}, isSelfClosing: true, filename: 'app', isComponent: false}" src="/vite.svg" class="logo" alt="Vite logo" />
-      <span :mometa="{ start: { column: 7, line: 9, offset: 223}, end: { column: 23, line: 9, offset: 239}, isSelfClosing: false, filename: 'app', isComponent: false}">asd</span><span :mometa="{ start: { column: 23, line: 9, offset: 239}, end: { column: 39, line: 9, offset: 255}, isSelfClosing: false, filename: 'app', isComponent: false}">asd</span>
+  <div :mometa="{ start: { column: 3, line: 6, offset: 102}, end: { column: 9, line: 14, offset: 401}, isSelfClosing: false, filename: 'app', isComponent: false, name: 'div'}">
+    <a :mometa="{ start: { column: 5, line: 7, offset: 112}, end: { column: 9, line: 10, offset: 264}, isSelfClosing: false, filename: 'app', isComponent: false, name: 'a'}" href="https://vitejs.dev" target="_blank">
+      <img :mometa="{ start: { column: 7, line: 8, offset: 164}, end: { column: 59, line: 8, offset: 216}, isSelfClosing: true, filename: 'app', isComponent: false, name: 'img'}" src="/vite.svg" class="logo" alt="Vite logo" />
+      <span :mometa="{ start: { column: 7, line: 9, offset: 223}, end: { column: 23, line: 9, offset: 239}, isSelfClosing: false, filename: 'app', isComponent: false, name: 'span'}">asd</span><span :mometa="{ start: { column: 23, line: 9, offset: 239}, end: { column: 39, line: 9, offset: 255}, isSelfClosing: false, filename: 'app', isComponent: false, name: 'span'}">asd</span>
     </a>
-    <a :mometa="{ start: { column: 5, line: 11, offset: 269}, end: { column: 9, line: 13, offset: 392}, isSelfClosing: false, filename: 'app', isComponent: false}" href="https://vuejs.org/" target="_blank">
-      <img :mometa="{ start: { column: 7, line: 12, offset: 321}, end: { column: 69, line: 12, offset: 383}, isSelfClosing: true, filename: 'app', isComponent: false}" src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    <a :mometa="{ start: { column: 5, line: 11, offset: 269}, end: { column: 9, line: 13, offset: 392}, isSelfClosing: false, filename: 'app', isComponent: false, name: 'a'}" href="https://vuejs.org/" target="_blank">
+      <img :mometa="{ start: { column: 7, line: 12, offset: 321}, end: { column: 69, line: 12, offset: 383}, isSelfClosing: true, filename: 'app', isComponent: false, name: 'img'}" src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
@@ -154,16 +154,16 @@ const list = ref([1, 2, 3, 4])
 </script>
 
 <template>
-  <div :mometa="{ start: { column: 3, line: 7, offset: 106}, end: { column: 9, line: 15, offset: 405}, isSelfClosing: false, filename: 'app', isComponent: false}">
-    <a :mometa="{ start: { column: 5, line: 8, offset: 116}, end: { column: 9, line: 11, offset: 268}, isSelfClosing: false, filename: 'app', isComponent: false}" href="https://vitejs.dev" target="_blank">
-      <img :mometa="{ start: { column: 7, line: 9, offset: 168}, end: { column: 59, line: 9, offset: 220}, isSelfClosing: true, filename: 'app', isComponent: false}" src="/vite.svg" class="logo" alt="Vite logo" />
-      <span :mometa="{ start: { column: 7, line: 10, offset: 227}, end: { column: 23, line: 10, offset: 243}, isSelfClosing: false, filename: 'app', isComponent: false}">asd</span><span :mometa="{ start: { column: 23, line: 10, offset: 243}, end: { column: 39, line: 10, offset: 259}, isSelfClosing: false, filename: 'app', isComponent: false}">asd</span>
+  <div :mometa="{ start: { column: 3, line: 7, offset: 106}, end: { column: 9, line: 15, offset: 405}, isSelfClosing: false, filename: 'app', isComponent: false, name: 'div'}">
+    <a :mometa="{ start: { column: 5, line: 8, offset: 116}, end: { column: 9, line: 11, offset: 268}, isSelfClosing: false, filename: 'app', isComponent: false, name: 'a'}" href="https://vitejs.dev" target="_blank">
+      <img :mometa="{ start: { column: 7, line: 9, offset: 168}, end: { column: 59, line: 9, offset: 220}, isSelfClosing: true, filename: 'app', isComponent: false, name: 'img'}" src="/vite.svg" class="logo" alt="Vite logo" />
+      <span :mometa="{ start: { column: 7, line: 10, offset: 227}, end: { column: 23, line: 10, offset: 243}, isSelfClosing: false, filename: 'app', isComponent: false, name: 'span'}">asd</span><span :mometa="{ start: { column: 23, line: 10, offset: 243}, end: { column: 39, line: 10, offset: 259}, isSelfClosing: false, filename: 'app', isComponent: false, name: 'span'}">asd</span>
     </a>
-    <a :mometa="{ start: { column: 5, line: 12, offset: 273}, end: { column: 9, line: 14, offset: 396}, isSelfClosing: false, filename: 'app', isComponent: false}" href="https://vuejs.org/" target="_blank">
-      <img :mometa="{ start: { column: 7, line: 13, offset: 325}, end: { column: 69, line: 13, offset: 387}, isSelfClosing: true, filename: 'app', isComponent: false}" src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    <a :mometa="{ start: { column: 5, line: 12, offset: 273}, end: { column: 9, line: 14, offset: 396}, isSelfClosing: false, filename: 'app', isComponent: false, name: 'a'}" href="https://vuejs.org/" target="_blank">
+      <img :mometa="{ start: { column: 7, line: 13, offset: 325}, end: { column: 69, line: 13, offset: 387}, isSelfClosing: true, filename: 'app', isComponent: false, name: 'img'}" src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <span :mometa="{ start: { column: 3, line: 16, offset: 408}, end: { column: 59, line: 16, offset: 464}, isSelfClosing: false, filename: 'app', isComponent: false}" v-for="item in list" :key="item">{{ item }}</span>
+  <span :mometa="{ start: { column: 3, line: 16, offset: 408}, end: { column: 59, line: 16, offset: 464}, isSelfClosing: false, filename: 'app', isComponent: false, name: 'span'}" v-for="item in list" :key="item">{{ item }}</span>
 </template>
 
 <style scoped>
@@ -212,8 +212,8 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <HelloWorld msg="Vite + Vue" />
   <hello-world msg="Vite + Vue" />
-  <a-button :mometa="{ start: { column: 3, line: 8, offset: 171}, end: { column: 53, line: 8, offset: 221}, isSelfClosing: false, filename: 'app', isComponent: true}" type="primary">Primary Button</a-button>
-  <div :mometa="{ start: { column: 3, line: 9, offset: 224}, end: { column: 20, line: 9, offset: 241}, isSelfClosing: false, filename: 'app', isComponent: false}">sadasd</div>
+  <a-button :mometa="{ start: { column: 3, line: 8, offset: 171}, end: { column: 53, line: 8, offset: 221}, isSelfClosing: false, filename: 'app', isComponent: true, name: 'a-button'}" type="primary">Primary Button</a-button>
+  <div :mometa="{ start: { column: 3, line: 9, offset: 224}, end: { column: 20, line: 9, offset: 241}, isSelfClosing: false, filename: 'app', isComponent: false, name: 'div'}">sadasd</div>
 </template>
 
 <style scoped>
@@ -262,7 +262,7 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <HelloWorld msg="Vite + Vue">
     <template #icon>
-      <div :mometa="{ start: { column: 7, line: 8, offset: 159}, end: { column: 21, line: 8, offset: 173}, isSelfClosing: false, filename: 'app', isComponent: false, slotName: 'icon'}">asd</div>
+      <div :mometa="{ start: { column: 7, line: 8, offset: 159}, end: { column: 21, line: 8, offset: 173}, isSelfClosing: false, filename: 'app', isComponent: false, slotName: 'icon', name: 'div'}">asd</div>
     </template>
   </HelloWorld>
 </template>
