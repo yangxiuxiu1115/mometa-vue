a964eec59e162faa69945d316c135a36845fa6b4
RicardoErii <1974364190@qq.com>
2023-06-02 23:41:20 +0800

    build: 构建命令修复

--- packages/editor/scripts/build.ts
+++ packages/editor/scripts/build.ts
@@ -1,6 +1,6 @@
 import { execSync } from 'node:child_process'
 
-const commands = ['pnpm run build-editor', 'pnpm run build-plugin']
+const commands = ['pnpm run build:editor', 'pnpm run build:plugin']
 
 for (const command of commands) {
   execSync(command, { stdio: 'inherit' })
